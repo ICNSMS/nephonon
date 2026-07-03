b174ba33431d7e627ab35420499d466e87b5d3d9
suan12 <627138453@qq.com>
2026-07-03 11:07:16 +0800

    Add files via upload

---
+++ example/graphene/band_plot.m
@@ -0,0 +1,30 @@
+function band_plot
+%% clean everythings
+clear; clc; close all;
+
+%% Read data
+data = importdata('phononband.dat', ' ', 1);  % 自动跳过第一行（标题行），空格分隔
+
+x = data.data(:, 1);   % 第一列：qpath
+y = data.data(:, 2);   % 第二列：Energy (meV)
+
+%% Plot
+figure;
+plot(x, y, 'o', 'MarkerSize', 1.0, 'Color', 'b');
+hold on;
+
+xlim([0, max(x)]);
+ylim([0, max(y)]);
+
+line([x(200), x(200)], [0,max(y)], 'LineStyle', '-', 'LineWidth', 1.5);
+line([x(450), x(450)], [0,max(y)], 'LineStyle', '-', 'LineWidth', 1.5);
+
+set(gca, 'XTick', [0.0,x(200), x(450), max(x)]);
+set(gca, 'XTickLabel', {'\Gamma','M', 'K', '\Gamma'});
+
+ylabel('Energy (meV)');
+title('Band Structure of graphene');
+grid on;
+grid minor;  % 可选，显示细网格
+
+end
